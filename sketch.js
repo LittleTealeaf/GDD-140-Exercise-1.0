175200aa757875b2c1dbfdb5af98aab244d3923b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-02-01 15:31:22 -0500

    class write-along

--- sketch.js
+++ sketch.js
@@ -2,8 +2,24 @@
 
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(255,0,0);
+  background(220);
+  strokeWeight(4);
+  stroke(0);
+  line(0,0,400,400);
+  line(0,400,400,0);
+  line(0,0,400,0);
+  line(0,0,0,400);
+  line(400,400,0,400);
+  line(400,400,400,0);
+  strokeWeight(1);
+  fill(255,0,0);
+  rect(200,200,100,100);
+  fill(255,255,0);
+  rect(width/2,height/2,75,75);
+  fill(0,255,100);
+  ellipse(width/2,height/2,70,40);
 }
